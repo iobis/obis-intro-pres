5ce26b1f856e184d808468d3d50ed40a7084c75d
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2025-02-08 14:25:23 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,20 @@
 # Install packages
-message("Installing packages")
-install.packages(c("terra", "sf", "tidyverse", "devtools", "robis", "arrow", "sfarrow", "stars", "patchwork"))
-devtools::install_github('bio-oracle/biooracler')
+message("Installing packages from CRAN")
+install.packages("tidyverse")        # Tidyverse
+install.packages(c("terra", "sf"))   # Spatial
+install.packages(c("DBI", "duckdb")) # Database
+install.packages(c(                  # Others
+    "arrow",
+    "devtools",
+    "robis", 
+    "rnaturalearth",
+    "mgcv",
+    "gifski",
+    "fs",
+    "h3jsr"
+
+))
+# Install those from GitHub
+message("Installing packages from GitHub")
+devtools::install_github("iobis/obistools")
+devtools::install_github("ropensci/mregions2")
